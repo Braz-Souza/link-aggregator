029f3ad60d13486d7e168fb774ce227156fb2896
Braz Souza <brazsdsgabriel@gmail.com>
2025-06-19 20:35:06 -0300

    Add AuthCheck

---
+++ src/routes/components/AuthCheck.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { user } from "$lib/firebase";
+  interface Props {
+    children?: import("svelte").Snippet;
+  }
+
+  let { children } = $props();
+</script>
+
+{#if $user}
+  {@render children?.()}
+{:else}
+  <p class="text-error my-10 flex flex-col m-4 md:m-10 gap-5">
+    You must be signed in to view this page.
+    <a class="btn preset-tonal-error" href="/login">Sign in</a>
+  </p>
+{/if}
--- src/routes/login/photo/+page.svelte
+++ src/routes/login/photo/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
+  import AuthCheck from "../../components/AuthCheck.svelte";
 </script>
 
-<article class="space-y-4 p-4">
-  <div>
-    <h2 class="h6">Photos</h2>
-    <h3 class="h3">Skeleton is Awesome</h3>
-  </div>
-  <p class="opacity-60">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque consequatur tempore sint
-    nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae rem.
-  </p>
-</article>
+<AuthCheck>
+  <article class="space-y-4 p-4">
+    <div>
+      <h2 class="h6">Photos</h2>
+      <h3 class="h3">Skeleton is Awesome</h3>
+    </div>
+    <p class="opacity-60">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+      aspernatur provident eveniet eligendi cumque consequatur tempore sint nisi
+      sapiente. Iste beatae laboriosam iure molestias cum expedita architecto
+      itaque quae rem.
+    </p>
+  </article>
+</AuthCheck>
