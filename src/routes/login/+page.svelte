12f74804020511953535c8abeb90fbb6686cf17a
Braz Souza <brazsdsgabriel@gmail.com>
2025-06-19 20:25:38 -0300

    Add Auth Store to display info after login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { auth } from "$lib/firebase";
+  import { auth, user } from "$lib/firebase";
 
   import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
@@ -11,19 +11,30 @@
 </script>
 
 <article class="space-y-4 p-4">
-  <div>
-    <h2 class="h6">User Login</h2>
-    <h3 class="h3">Skeleton is Awesome</h3>
-  </div>
-  <p class="opacity-60">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur
-    provident eveniet eligendi cumque consequatur tempore sint nisi sapiente.
-    Iste beatae laboriosam iure molestias cum expedita architecto itaque quae
-    rem.
-  </p>
-  <button
-    type="button"
-    onclick={signInWithGoogle}
-    class="btn preset-filled-primary-500">Sign in with Google</button
-  >
+  {#if $user}
+    <div>
+      <h2 class="h6">Welcome {$user.displayName}</h2>
+      <h3 class="h3">User is Logged in</h3>
+    </div>
+    <p class="opacity-60">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+      aspernatur provident eveniet eligendi cumque consequatur tempore sint nisi
+      sapiente. Iste beatae laboriosam iure molestias cum expedita architecto
+      itaque quae rem.
+    </p>
+    <button
+      type="button"
+      onclick={() => signOut(auth)}
+      class="btn preset-filled-warning-500">Sign out</button
+    >
+  {:else}
+    <div>
+      <h2 class="h6">User Login</h2>
+    </div>
+    <button
+      type="button"
+      onclick={signInWithGoogle}
+      class="btn preset-filled-primary-500">Sign in with Google</button
+    >
+  {/if}
 </article>
