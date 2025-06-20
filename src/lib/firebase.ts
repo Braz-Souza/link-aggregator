3362e1ab708e7f2e04a712310b91a16046bd51cd
Braz Souza <brazsdsgabriel@gmail.com>
2025-06-19 21:29:00 -0300

    Add Custom Usernames

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { 
-  PUBLIC_FBCLIENT_API_KEY, 
+import {
+  PUBLIC_FBCLIENT_API_KEY,
   PUBLIC_FBCLIENT_AUTH_DOMAIN,
   PUBLIC_FBCLIENT_PROJECT_ID,
   PUBLIC_FBCLIENT_STORAGE_BUCKET,
   PUBLIC_FBCLIENT_MESSAGING_SENDER_ID,
-  PUBLIC_FBCLIENT_APP_ID 
+  PUBLIC_FBCLIENT_APP_ID,
 } from "$env/static/public";
-import { writable } from "svelte/store";
+import { derived, writable, type Readable } from "svelte/store";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,7 +20,7 @@ const firebaseConfig = {
   projectId: PUBLIC_FBCLIENT_PROJECT_ID,
   storageBucket: PUBLIC_FBCLIENT_STORAGE_BUCKET,
   messagingSenderId: PUBLIC_FBCLIENT_MESSAGING_SENDER_ID,
-  appId: PUBLIC_FBCLIENT_APP_ID
+  appId: PUBLIC_FBCLIENT_APP_ID,
 };
 
 // Initialize Firebase
@@ -36,11 +36,11 @@ function userStore() {
   let unsubscribe: () => void;
 
   if (!auth || !globalThis.window) {
-    console.warn('Auth is not initialized or not in browser');
+    console.warn("Auth is not initialized or not in browser");
     const { subscribe } = writable<User | null>(null);
     return {
       subscribe,
-    }
+    };
   }
 
   const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
@@ -57,3 +57,42 @@ function userStore() {
 }
 
 export const user = userStore();
+
+/**
+ * @param  {string} path document path or reference
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(path: string) {
+  const docRef = doc(db, path);
+  const { subscribe } = writable<T | null>(null, (set) => {
+    const unsubscribe = onSnapshot(docRef, (snapshot) => {
+      set((snapshot.data() as T) ?? null);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+    ref: docRef,
+    id: docRef.id,
+  };
+}
+
+interface UserData {
+  username: string;
+  bio: string;
+  photoURL: string;
+  links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(
+  user,
+  ($user, set) => {
+    if ($user) {
+      return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+    } else {
+      set(null);
+    }
+  }
+);
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,18 +1,127 @@
 <script lang="ts">
   import AuthCheck from "../../components/AuthCheck.svelte";
+  import { db, user, userData } from "$lib/firebase";
+  import { doc, getDoc, writeBatch } from "firebase/firestore";
+  let username = $state("");
+  let loading = $state(false);
+  let isAvailable = $state(false);
+  let debounceTimer: NodeJS.Timeout;
+
+  const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+  let isValid = $derived(
+    username?.length > 2 && username.length < 16 && re.test(username)
+  );
+  let isTouched = $derived(username.length > 0);
+  let isTaken = $derived(isValid && !isAvailable && !loading);
+
+  function checkAvailability() {
+    isAvailable = false;
+    clearTimeout(debounceTimer);
+    if (!isValid) {
+      loading = false;
+      return;
+    }
+
+    loading = true;
+
+    debounceTimer = setTimeout(async () => {
+      console.log("checking availability of", username);
+
+      const ref = doc(db, "usernames", username);
+      const exists = await getDoc(ref).then((doc) => doc.exists());
+
+      isAvailable = !exists;
+      loading = false;
+    }, 500);
+  }
+
+  async function confirmUsername(e: SubmitEvent) {
+    e.preventDefault();
+    console.log("confirming username", username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), {
+      uid: $user?.uid,
+    });
+    batch.set(doc(db, "users", $user!.uid), {
+      username,
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: "I am testing",
+      links: [
+        {
+          title: "My Website",
+          url: "https://example.com",
+          icon: "globe",
+        },
+        {
+          title: "My GitHub",
+          url: "https://github.com",
+          icon: "github",
+        },
+      ],
+    });
+
+    await batch.commit();
+    console.log("Username confirmed!");
+
+    username = "";
+    isAvailable = false;
+  }
 </script>
 
 <AuthCheck>
-  <article class="space-y-4 p-4">
-    <div>
-      <h2 class="h6">Username</h2>
-      <h3 class="h3">Skeleton is Awesome</h3>
-    </div>
-    <p class="opacity-60">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-      aspernatur provident eveniet eligendi cumque consequatur tempore sint nisi
-      sapiente. Iste beatae laboriosam iure molestias cum expedita architecto
-      itaque quae rem.
-    </p>
-  </article>
+  {#if $userData?.username}
+    <article class="space-y-4 p-4">
+      <p class="text-lg">
+        Your username is <span class="text-success-500 font-bold"
+          >@{$userData.username}</span
+        >
+      </p>
+      <p class="text-sm text-warning-500">(Usernames cannot be changed)</p>
+      <a class="btn preset-filled-primary-500" href="/login/photo"
+        >Upload Profile Image</a
+      >
+    </article>
+  {:else}
+    <article class="space-y-4 p-4">
+      <div>
+        <h2 class="h6">Username</h2>
+      </div>
+      <form onsubmit={confirmUsername}>
+        <input
+          type="text"
+          bind:value={username}
+          oninput={checkAvailability}
+          class="input"
+          placeholder="Enter your username"
+        />
+        <div class=" min-h-8 px-8 w-full">
+          {#if loading}
+            <p class="text-warning-500">
+              Checking availability of @{username}...
+            </p>
+          {/if}
+
+          {#if !isValid && isTouched}
+            <p class="text-error-500 text-sm">
+              must be 3-16 characters long, alphanumeric only
+            </p>
+          {/if}
+
+          {#if isValid && !isAvailable && !loading}
+            <p class="text-warning-500 text-sm">
+              @{username} is not available
+            </p>
+          {/if}
+
+          {#if isAvailable}
+            <button type="submit" class="btn mt-6 preset-filled-primary-500">
+              Confirm username @{username}
+            </button>
+          {/if}
+        </div>
+      </form>
+    </article>
+  {/if}
 </AuthCheck>
